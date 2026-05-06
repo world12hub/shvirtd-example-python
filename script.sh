748a4be9bf58d50d18cffaaf841f4974ccebf78d
Sergeykanyugin <112606666+Sergeykanyugin@users.noreply.github.com>
2026-05-06 20:02:03 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git clone https://github.com/YOUR_USERNAME/YOUR_FORK_REPO.git /opt/myproject
+cd /opt/myproject
+cat > .env <<EOF
+MYSQL_ROOT_PASSWORD="YtReWq4321"
+MYSQL_DATABASE="virtd"
+MYSQL_USER="app"
+MYSQL_PASSWORD="QwErTy1234"
+EOF
